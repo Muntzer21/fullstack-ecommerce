2aed6a97fc2ccc874c6680c08d4355828200484a
muntazer <muntzrdeveloper@gmail.com>
2026-08-19 11:04:28 +0300

    edit mail module

--- ecommerce-api/src/mail/mail.module.ts
+++ ecommerce-api/src/mail/mail.module.ts
@@ -1,29 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { MailerModule } from '@nestjs-modules/mailer';
+import { ConfigModule } from '@nestjs/config';
+
 import { MailService } from './mail.service';
 
 @Module({
-  imports: [
-    ConfigModule,
-    MailerModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        transport: {
-          host: config.get<string>('MAIL_HOST'),
-          port: Number(config.get('MAIL_PORT')),
-          secure: false,
-          auth: {
-            user: config.get<string>('MAIL_USER'),
-            pass: config.get<string>('MAIL_PASSWORD'),
-          },
-        },
-        defaults: {
-          from: config.get<string>('MAIL_FROM'),
-        },
-      }),
-    }),
-  ],
+  imports: [ConfigModule],
   providers: [MailService],
   exports: [MailService],
 })
